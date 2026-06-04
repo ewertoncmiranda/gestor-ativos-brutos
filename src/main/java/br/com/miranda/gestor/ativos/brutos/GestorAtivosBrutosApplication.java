de3da01f25c272a86d9ded056652ff9fc3f39f98
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2026-06-04 16:13:57 -0300

    teste run esteira

--- src/main/java/br/com/miranda/gestor/ativos/brutos/GestorAtivosBrutosApplication.java
+++ src/main/java/br/com/miranda/gestor/ativos/brutos/GestorAtivosBrutosApplication.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 
 import static br.com.miranda.gestor.ativos.brutos.tools.ConstantesUtils.MAIN;
 
